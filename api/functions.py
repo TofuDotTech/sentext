7f4c4adb09a8988e1bf088137b2531693a30bcf2
Carloslecval <carloslecval03@gmail.com>
2023-11-25 20:52:07 -0600

    emotion and sentiment predictions

--- api/functions.py
+++ api/functions.py
@@ -2,14 +2,14 @@ import re
 import nltk
 from nltk import word_tokenize
 from nltk.corpus import stopwords
+import spacy
+from nltk.stem.wordnet import WordNetLemmatizer
 nltk.download('punkt')
 nltk.download('wordnet')
 nltk.download('omw-1.4')
-from nltk.stem.wordnet import WordNetLemmatizer
 lemmatizer = WordNetLemmatizer()
 nltk.download('stopwords')
 stop_words_en = set(stopwords.words('english'))
-import spacy
 nlp = spacy.load('en_core_web_lg')
 
 
@@ -19,11 +19,11 @@ def vectorize(text):
     text = text.strip()
     text = text.replace('\n', '')
     text = text.replace('\t', '')
-    text = re.sub(r'[^\w\s]','',text)
+    text = re.sub(r'[^\w\s]', '', text)
     text = word_tokenize(text)
     text = [palabra for palabra in text if palabra not in stop_words_en]
     text = [lemmatizer.lemmatize(palabra) for palabra in text]
     text = ' '.join(text)
     vector = nlp(text).vector
-    vector = vector.reshape(1,300,1)
+    vector = vector.reshape(1, 300, 1)
     return vector
--- api/model.py
+++ api/model.py
@@ -1,11 +1,39 @@
-nltk.download('punkt')
-nltk.download('wordnet')
-nltk.download('omw-1.4')
-nltk.download('stopwords')
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+from functions import vectorize
+
+sentiment_model = tf.keras.models.load_model(
+    '../modelo/sentiment_classifier.h5')
+emotion_model = tf.keras.models.load_model('../modelo/model_emotion.h5')
+
+sentiment_map = ['neutral', 'negative', 'positive']
+emotion_map = ['sadness', 'joy', 'love', 'anger', 'fear']
 
 
 def predict_sentiment(text):
-    pass
+    vector = vectorize(text)
+    prediction = np.argmax(sentiment_model.predict(vector), axis=-1)[0]
+    return sentiment_map[prediction]
 
 
-predict_sentiment('Hello')
+def predict_emotion(text):
+    vector = vectorize(text)
+    prediction = np.argmax(emotion_model.predict(vector), axis=-1)[0]
+    return emotion_map[prediction]
+
+
+def overall_sentiment(texts):
+    predictions = []
+    for text in texts:
+        predictions.append(predict_sentiment(text))
+    serie = pd.Series(predictions)
+    return serie.value_counts().index[0]
+
+
+def overall_emotion(texts):
+    predictions = []
+    for text in texts:
+        predictions.append(predict_emotion(text))
+    serie = pd.Series(predictions)
+    return serie.value_counts().index[0]
