43c987834f0eec6c3de6831a6472408f8575954a
Carloslecval <carloslecval03@gmail.com>
2023-11-26 18:16:58 -0600

    rag endpoint

--- api/main.py
+++ api/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from flask_cors import CORS
 import uuid
+from genRag import answerQuestion
 from model import overall_emotion, overall_sentiment
 
 from genAi import genDescription
@@ -18,6 +19,15 @@ def predictEmotion():
     sentiment = overall_sentiment(json['comments'])
     emotion = overall_emotion(json['comments'])
     return {"message": result, 'sentiment': sentiment, 'emotion': emotion}, 200
+
+
+@app.route("api/query", method=["POST"])
+def answerQuery():
+    json = request.json
+    if (json == None):
+        return {"success": False}
+    result = answerQuestion(json["query"])
+    return {"answer": result}, 200
 
 
 if __name__ == "__main__":
