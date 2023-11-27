6b227c87a6f407b282bf1f9ff237a076be025474
Iván Vega <ivanchovino@outlook.com>
2023-11-26 19:23:23 -0600

    Finishes with the basic function of the extension

--- extension/index.js
+++ extension/index.js
@@ -1,15 +1,96 @@
-const title = document.querySelector("#title");
+const content = document.querySelector("#content");
+const titleImg = document.querySelector("#title-img");
+const description = document.querySelector("#description");
 const button = document.querySelector("#main-button");
+
+// After analyze
+const emotion = document.querySelector("#emotion");
+const summary = document.querySelector("#summary");
+const summaryContent = document.querySelector("#summary-content");
+const responseHeader = document.querySelector("#response-header");
+const handImg = document.querySelector("#hand-img");
+const handCircle = document.querySelector("#hand-circle");
+const sentiment = document.querySelector("#sentiment");
+const chatDiv = document.querySelector("#chat-div");
+const chatPrompt = document.querySelector("#chat-prompt");
+const askButton = document.querySelector("#ask-button");
+const anotherContent = document.querySelector("#another-content");
+
 const analyze = new CustomEvent("Analyze", { detail: "message" });
+let clickable = true;
+
+function modifyExtensionElements(messageText, sentimentText, emotionText) {
+  if (sentimentText === "neutral") {
+    document.body.style.backgroundColor = "#E8EBF1";
+    handCircle.classList.add("hand-circle-neutral");
+    handImg.classList.add("hand-img-neutral");
+    sentiment.textContent =
+      "Nothing to say. This post is getting a pretty neutral response";
+    sentiment.classList.add("sentiment-neutral");
+    askButton.classList.add("ask-neutral");
+    chatPrompt.classList.add("chat-prompt-neutral");
+  }
+
+  if (sentimentText === "positive") {
+    document.body.style.backgroundColor = "#E3EBE2";
+    handCircle.classList.add("hand-circle-positive");
+    handImg.classList.add("hand-img-positive");
+    sentiment.textContent = "Nice! This post is getting a positive response";
+    sentiment.classList.add("sentiment-positive");
+    askButton.classList.add("ask-positive");
+    chatPrompt.classList.add("chat-prompt-positive");
+  }
+
+  if (sentimentText === "negative") {
+    document.body.style.backgroundColor = "#F1E8EB";
+    handCircle.classList.add("hand-circle-negative");
+    handImg.classList.add("hand-img-negative");
+    sentiment.textContent = "Oops... This post is getting a negative response";
+    sentiment.classList.add("sentiment-negative");
+    askButton.classList.add("ask-negative");
+    chatPrompt.classList.add("chat-prompt-negative");
+  }
+
+  chatDiv.classList.remove("hidden");
+
+  titleImg.classList.add("hidden");
+  button.classList.add("hidden");
+
+  responseHeader.classList.remove("hidden");
+
+  content.classList.add("content-size-big");
+  content.classList.remove("content-size-normal");
+  description.classList.remove("loading-text");
+  description.classList.add("hidden");
+
+  emotion.textContent = emotionText;
+  summary.textContent = messageText;
+
+  summaryContent.style.display = "flex";
+  anotherContent.style.display = "flex";
+}
 
 chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
-  if (request.response) {
+  if (request.data) {
     console.log("Mensaje recibido en popup.js:", request.data);
+    modifyExtensionElements(
+      `Summary: ${request.data.message}`,
+      `${request.data.sentiment}`,
+      `Emotion: ${
+        request.data.emotion[0].toUpperCase() +
+        request.data.emotion.substring(1)
+      }`
+    );
+    clickable = true;
   }
 });
 
 button.addEventListener("click", () => {
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    chrome.tabs.sendMessage(tabs[0].id, { action: "analyze" });
-  });
+  if (clickable) {
+    clickable = false;
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      chrome.tabs.sendMessage(tabs[0].id, { action: "analyze" });
+      description.classList.add("loading-text");
+    });
+  }
 });
--- extension/scripts/x_script.js
+++ extension/scripts/x_script.js
@@ -11,25 +11,37 @@ chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
 
     console.log(comments);
 
-    const response = await fetch("http://127.0.0.1:5000/api/emotions", {
+    /* const response = await fetch("http://127.0.0.1:5000/api/emotions", {
       method: "POST",
       headers: {
         Accept: "application/json",
         "Content-Type": "application/json",
       },
-      body: {
+      body: JSON.stringify({
         comments: comments,
         platform: "X",
-      },
+      }),
     });
 
     response.json().then((data) => {
+      console.log(data);
+      console.log();
       console.log(JSON.stringify(data));
 
       chrome.runtime.sendMessage({
-        // response: "Message from content_script.js",
         data: data,
       });
-    });
+    }); */
+
+    setTimeout(() => {
+      chrome.runtime.sendMessage({
+        data: {
+          message:
+            "This is a test that is going to determine if asdfkjasdf kasdfjk ajdkfjasdfk ajksdfjlk alsdfasdkf jkasdfjkasdf jalsdjflka jkjflaksjdflk jalskdfj kajsdflkj alksjdflkjasd jasjdfkl jaslkdfjlk ajsdkfj lkajsdlkfj aklsdjflk jaslkdfjlk jalkdfjlka jlkdjfa sz",
+          sentiment: "positive",
+          emotion: "happiness",
+        },
+      });
+    }, 2000);
   }
 });
